18c9e31ba969b48370487145bd97f5b9ceeb9a72
Adamovska Vika <vikusya.ad@gmail.com>
2019-06-02 00:18:58 +0300

    new version

--- src/edu/hillel/innerclass/InnerClassDemo.java
+++ src/edu/hillel/innerclass/InnerClassDemo.java
@@ -9,31 +9,46 @@ interface  Test{
 interface Cat{
 
 }
+
+
+
 class Dog implements Cat{
 
     private int privateVar = 10;
     private static int staricVar = 3;
+
+
+    ///////////////////////////////////////////////////////////
+
 
     private class InnerDog implements Test{
         public int getParentPrivateVar(){
             return privateVar;
         }
     }
-
+//////////////////////////////////
 
 
     public static class InnerDogStatic{
 
         public int getOuterStaticField(){
+
             return staricVar;
         }
 
     }
+
+
+
+
     public Test getPrivateObj(){
+
         return new InnerDog();
     }
 }
 
+
+//////////////////////////////////////////////////////
 public class InnerClassDemo {
 
 
